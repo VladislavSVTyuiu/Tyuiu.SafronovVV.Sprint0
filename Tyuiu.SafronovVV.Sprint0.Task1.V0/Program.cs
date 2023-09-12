c014ccdce020a6dad851f25710999aaaadeaca06
X-Ray2003 <134064048+X-Ray2003@users.noreply.github.com>
2023-09-12 15:34:37 +0500

    Task1

---
+++ Tyuiu.SafronovVV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SafronovVV.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello, World ");
+            string name = "Влад ";
+            int age = 20;
+            Console.WriteLine(" Доброго времени суток, меня зовут " + name + "и мне " + age + " лет ");
+            Console.ReadKey();
+        }
+    }
+}
